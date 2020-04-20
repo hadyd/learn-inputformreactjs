e2bb8edb3e2ef90a565602b2063fa65ed9761493
hadyd <anr.hadyd@gmail.com>
2020-04-20 13:38:30 +0700

    Update

--- src/components/MessagePage.js
+++ src/components/MessagePage.js
@@ -2,20 +2,17 @@ import React from "react";
 
 function MessagePage() {
  
-//   const handleClick = () => {
-//     document.getElementById("result").innerHTML = localStorage.getItem("user");  
-//   };  
+  const handleClick = () => {
+    document.getElementById("result").innerHTML = localStorage.getItem("user");  
+  };  
 
-//   return (
-//     <div className="form-contact">
-//       <h3>Halaman Pesan</h3>
-//       <h5 id ="result"></h5>
-//       <button onClick={handleClick}>Get Message</button>
-//     </div>
-//   );
-
-
-
+  return (
+    <div className="form-contact">
+      <h3>Halaman Pesan</h3>
+      <h5 id ="result"></h5>
+      <button onClick={handleClick}>Get Message</button>
+  </div>
+  );
 
 }
 
